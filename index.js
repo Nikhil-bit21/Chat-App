6060a2a6b74df9c529ff6b4ea2ce05fae8851ed7
Nikhil-bit21 <swaminikhil21@gmail.com>
2026-08-27 23:41:20 +0530

    Day 1 of learning

--- index.js
+++ index.js
@@ -8,15 +8,28 @@ const app = express();
 app.use(express.static(path.resolve('./public')));
 
 const server = http.createServer(app); 
-const io = new Server(server);
+const io = new Server(server,{
+    connectionStateRecovery:{}
+});
 
 const PORT = 3000;
 
 //Socket Connection (socket is basically client)
 io.on('connection',(socket)=>{
     socket.on('user-msg',message=>{
-        io.emit('msg',message);
+        //io.emit('msg',message); // sends/emits message to everyone including itself
+        socket.broadcast.emit('msg',message); // for sending/emitting message to everyone except itself
     })
+    socket.on('disconnect',()=>{
+        // console.log('user disconnected');
+    })
+    // socket.timeout(5000).emit('wait-message',{name:'Nikhil'},(err,res)=>{
+    //     if(err){
+    //         console.log('Client did not responded ')
+    //     }else{
+    //         console.log(res.status);
+    //     }
+    // })
 })
 
 app.get('/',(req,res)=>{
