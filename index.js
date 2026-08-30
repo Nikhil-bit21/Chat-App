c22628607ea6d58ac7c8688173692998b9cb87ac
Nikhil-bit21 <swaminikhil21@gmail.com>
2026-08-30 17:27:22 +0530

    basic is done will visit it later

--- index.js
+++ index.js
@@ -2,8 +2,25 @@ const express = require('express');
 const http = require('http');
 const path = require('path');
 const {Server} = require('socket.io');
+const sqlite3 = require('sqlite3');
+const {open} = require('sqlite');
 
 const app = express();
+
+let db;
+
+async function dbConnection(){
+    db = await open({
+        filename:'chat.db',
+        driver:sqlite3.Database
+    })
+
+    await db.exec(`Create Table IF Not Exists messages(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        client_offset TEXT UNIQUE,
+        content text
+        )`)
+}
 
 app.use(express.static(path.resolve('./public')));
 
@@ -15,14 +32,32 @@ const io = new Server(server,{
 const PORT = 3000;
 
 //Socket Connection (socket is basically client)
-io.on('connection',(socket)=>{
-    socket.on('user-msg',message=>{
+io.on('connection',async (socket)=>{
+    socket.on('user-msg',async (message)=>{
         //io.emit('msg',message); // sends/emits message to everyone including itself
-        socket.broadcast.emit('msg',message); // for sending/emitting message to everyone except itself
+        // socket.broadcast.emit('msg',message); // for sending/emitting message to everyone except itself
+        let res;
+        try{
+            res = await db.run('INSERT INTO messages (content) values (?)',message);
+        }catch(e){
+            return;
+        }
+        socket.broadcast.emit('msg',message,res.lastID);
     })
-    socket.on('disconnect',()=>{
-        // console.log('user disconnected');
-    })
+    if(!socket.recovered){
+        try{
+            await db.each('select id,content from messages where id>?',[socket.handshake.auth.serverOffset || 0],
+                (_err , row)=>{
+                    socket.emit('msg',row.content,row.id);
+                }
+            )
+        }catch(e){
+
+        }
+    }
+    // socket.on('disconnect',()=>{
+    //     // console.log('user disconnected');
+    // })
     // socket.timeout(5000).emit('wait-message',{name:'Nikhil'},(err,res)=>{
     //     if(err){
     //         console.log('Client did not responded ')
@@ -36,6 +71,8 @@ app.get('/',(req,res)=>{
     return res.sendFile('/public/index.html');
 })
 
-server.listen(PORT,()=>{
+dbConnection().then(()=>{
+    server.listen(PORT,()=>{
     console.log(`Server started at Port ${PORT}`);
 })
+});
